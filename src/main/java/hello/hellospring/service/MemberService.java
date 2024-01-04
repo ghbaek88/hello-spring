53b752b35b6fe2b523e4f81dc352170d6cff9975
백기환 <ghbaek88@naver.com>
2024-01-04 15:29:07 +0900

    스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 섹션 6-2 ~ 7

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,11 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -22,10 +23,9 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member){
-        validateDuplicateMember(member); // 중복회원 검증
-
-        memberRepository.save(member);
-        return member.getId();
+            validateDuplicateMember(member); // 중복회원 검증
+            memberRepository.save(member);
+            return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
